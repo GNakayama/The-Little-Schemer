4de692e82f744cc34d766066b7bb9e2f7cd26d74
Guilherme Nakayama <guilherme.nakayama.s@gmail.com>
2018-11-18 14:57:34 -0200

    Implements member? function

--- aux.rkt
+++ aux.rkt
@@ -8,3 +8,10 @@
       ((null? l) #t)
       ((atom? (car l)) (lat? (cdr x)))
       (else #f))))
+
+(define member?
+  (lambda (a l)
+    (cond
+      ((null? l) #f)
+      ((eq? a (car l)) #t)
+      (else (member? a (cdr l))))))
